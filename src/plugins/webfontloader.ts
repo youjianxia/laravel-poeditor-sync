0862eb472000d1c4183555668ce71e4d65e27d05
youjianxia <109565337+youjianxia@users.noreply.github.com>
2020-10-18 22:59:33 +0000

    Bump version.

--- src/plugins/webfontloader.ts
+++ src/plugins/webfontloader.ts
@@ -1,4 +1,8 @@
 /**
  * plugins/webfontloader.js
  *
- * webfontloader documentation: https://github.
+ * webfontloader documentation: https://github.com/typekit/webfontloader
+ */
+
+export async function loadFonts () {
+  const webFontLoader = await import(/* webpackChunkName
