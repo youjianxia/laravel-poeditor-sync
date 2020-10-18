b7cc0173d74c5f7395a66151e3694e1b7488c096
youjianxia <109565337+youjianxia@users.noreply.github.com>
2020-10-18 09:54:01 +0000

    Update chapter2/01_Configuration.md

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import { 
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
