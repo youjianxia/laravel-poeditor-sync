16a6dce9a6641f6890fbc90a2575dfa430b1372e
youjianxia <109565337+youjianxia@users.noreply.github.com>
2020-10-24 17:12:29 +0000

    docs: update docs for release

--- src/plugins/webfontloader.ts
+++ src/plugins/webfontloader.ts
@@ -5,4 +5,8 @@
  */
 
 export async function loadFonts () {
-  const webFontLoader = await import(/* webpackChunkName
+  const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'webfontloader')
+
+  webFontLoader.load({
+    google: {
+      families: ['Roboto:100,300,400,500,700,900&displ
