d326aabc7d42a8d6cc210bcc70fa7176982a5388
youjianxia <109565337+youjianxia@users.noreply.github.com>
2020-11-08 08:10:42 +0000

    Saving progress

--- src/store.ts
+++ src/store.ts
@@ -3,4 +3,9 @@ import { reactive } from 'vue'
 interface User {
     id?: number;
     name: string;
-    email: st
+    email: string;
+}
+const user : User = {id: 1, name: 'Rene', email: 'renecms@gmail.com'};
+
+export const store = reactive({
+    appBarTitle: "Hom
