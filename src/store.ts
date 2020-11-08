7070174bdae37b845c6324f09e6eae270122bb89
youjianxia <109565337+youjianxia@users.noreply.github.com>
2020-11-08 12:00:28 +0000

    JavaDoc update

--- src/store.ts
+++ src/store.ts
@@ -8,4 +8,10 @@ interface User {
 const user : User = {id: 1, name: 'Rene', email: 'renecms@gmail.com'};
 
 export const store = reactive({
-    appBarTitle: "Hom
+    appBarTitle: "Home",
+    user,
+    loggedIn: true,
+    changeAppBarTitle(newTitle: string) {
+        this.appBarTitle = newTitle;
+    },
+    changeUser(newUser: User
