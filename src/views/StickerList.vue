3bd1028ea5e4a2f8b60e981497393127de0231e7
youjianxia <109565337+youjianxia@users.noreply.github.com>
2020-12-06 12:05:16 +0000

    Update Images

--- src/views/StickerList.vue
+++ src/views/StickerList.vue
@@ -104,4 +104,5 @@ export default {
       const filterOwned = (list: Array<Sticker>) => {
         return this.showOwned ? list.filter(sticker => sticker.owned != this.showOwned) : list
       }
-  
+      const filterName = (list: Array<Sticker>) => {
+        return this.searchTerm ? list.filter(sticker => sticker.nam
