5e69fbddeaa63d85dbef231f56b526a04d6b85e8
youjianxia <109565337+youjianxia@users.noreply.github.com>
2021-01-30 18:07:49 +0000

    More documentation changes

--- src/views/TradePoint.vue
+++ src/views/TradePoint.vue
@@ -4,4 +4,9 @@
       <v-text-field
         v-model="searchTerm"
         label="Busca por nome do ponto de troca"
-  
+      />
+    </v-col>
+    <v-col cols="3">
+      <v-checkbox
+        v-model="showLinkedOnly"
+        label="Mostrar apenas pontos frequentad
