cc4a497354d77e3bbfa70006c2c55480b0d49917
youjianxia <109565337+youjianxia@users.noreply.github.com>
2021-03-07 21:23:21 +0000

    cargo update

--- src/views/TradePoint.vue
+++ src/views/TradePoint.vue
@@ -92,4 +92,6 @@ export default {
 
   },
   methods: {
+    linkedChange(item: TradePoint) {
+      fetch(`https://stickers-trade-be-vqklpjxjja-rj.a.run.app/user/${store.user.id}/trade_point/${item.id}`, {
     
